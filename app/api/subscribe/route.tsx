75fce9f23eb94cef1ea97947dac2f61b62280bdb
lora <3526039967@qq.com>
2025-12-27 17:12:32 +0800

    feat: add subscribe feature use Resend provider

--- app/api/subscribe/route.tsx
+++ app/api/subscribe/route.tsx
@@ -1,7 +1,28 @@
+import { Resend } from "resend";
+import { NextResponse } from "next/server";
+
 export async function POST(request: Request) {
   const { email } = await request.json();
-  console.log(email);
-
+  console.log(email); //xxx@qq.com
+  // save to contact List
+  const resend = new Resend(process.env.RESEND_API_KEY);
+  // create a acounnt
+  const { error: CreateError } = await resend.contacts.create({
+    email: email,
+  });
+  if (CreateError) {
+    console.log(CreateError);
+    return NextResponse.json({ error: CreateError.message }, { status: 500 });
+  }
+  //  add acount to contact List
+  const { error: AddError } = await resend.contacts.segments.add({
+    email: email,
+    segmentId: "d7d093bd-99af-4ef9-b076-15b9999d105c",
+  });
+  if (AddError) {
+    console.log(AddError);
+    return NextResponse.json({ error: AddError.message }, { status: 500 });
+  }
   // Add your subscription logic here
-  return new Response("Subscribed", { status: 200 });
+  return NextResponse.json("Subscribed", { status: 200 });
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,37 @@
+"use client";
+
 import { BookOpenCheck } from "lucide-react";
 import Link from "next/link";
 import Card from "@/app/components/Card";
+import { useState } from "react";
+import { toast } from "sonner";
 export default function Home() {
+  const [email, setEmail] = useState("");
+  const [isSubscribed, setIsSubscribed] = useState(false);
+
+  const handleSubscribe = () => {
+    // Add your subscription logic here
+    fetch("/api/subscribe", {
+      method: "POST",
+      body: JSON.stringify({ email }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.error) {
+          toast.error(data.error);
+        } else {
+          setIsSubscribed(true);
+          toast.success("Subscribed successfully!");
+        }
+      })
+      .catch((error) => {
+        toast.error("An error occurred while subscribing.", error);
+      })
+      .finally(() => {
+        setEmail("");
+      });
+  };
+
   return (
     <div className="min-h-screen bg-white">
       {/** Header */}
@@ -37,17 +67,22 @@ export default function Home() {
       <div className="flex items-center text-center justify-center gap-4 ">
         <input
           type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
           placeholder="Enter your email"
           className="border border-gray-300 rounded-md px-4 py-2"
         />
-        <button className="bg-black text-white px-4 py-2 rounded-md hover:bg-blue-600">
+        <button
+          className="bg-black text-white px-4 py-2 rounded-md hover:bg-blue-600"
+          onClick={handleSubscribe}
+        >
           Subscribe
         </button>
       </div>
 
       {/*Card max-w-7xl 限制最大宽度，水平居中，内容上方的容器，左右各自添加32px的padding*/}
       <div className="max-w-7xl mx-auto px-8 ">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 ">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-16 ">
           <Card
             title="AI"
             description="How AI is transforming industries"
