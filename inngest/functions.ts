bdbfb5fe7fe38aac12b4c284c0e85cb00568d201
lora <3526039967@qq.com>
2025-12-27 18:33:15 +0800

    feat: add rss_util to process rss souce to display summart test and html

--- inngest/functions.ts
+++ inngest/functions.ts
@@ -8,3 +8,12 @@ export const helloWorld = inngest.createFunction(
     return { message: `Hello ${event.data.email}!` };
   },
 );
+
+export const sendDailyNews = inngest.createFunction(
+  { id: "send-daily-news" },
+  { event: "daily/news" },
+  async ({ event, step }) => {
+    await step.sleep("wait-a-moment", "1s");
+    return { message: `Daily news sent to ${event.data.email}!` };
+  },
+);
